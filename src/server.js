6e09baa7220ad7bb39b039c30bbb63e68eab37eb
Amanda dos Santos Silva <123501998+Amandamoonchild@users.noreply.github.com>
2023-11-09 17:01:16 -0300

    feat: CAbou

--- src/server.js
+++ src/server.js
@@ -8,8 +8,28 @@ const port = process.env.PORT || 5000; //Define a porta que será usada
 const app = express();// cria o servidor 
 app.use(express.json()); //permite que o servidor entenda requisições em json
 
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello, World!" })
+
+app.get("/:nome/:sobreNome/:idade/:signo", (req, res) => {
+    const nome = req.params.nome;
+    const sobreNome = req.params.sobreNome;
+    const idade= req.params.idade;
+    const sgin = req.params.signo;
+    res.status(200).json({"meu nome é": `${nome} ${sobreNome} ${idade}  ${sgin}`})
+});
+
+app.post("/", (req, res) =>{
+    const nome = req.body.nome;
+    const sobrenome = req.body.sobrenome;
+    const idade = req.body.idade;
+    res.status(200).json({ nomeUsuario: nome, sobrenomeUsuario: sobrenome, idadeUsuario: idade,})
+});
+
+app.put("/", (req, res) =>{
+    res.status(200).json({ message: "Hello, put" })
+});
+
+app.delete("/", (req, res) =>{
+    res.status(200).json({ message: "Hello, delete" })
 });
 
 app.listen(port, () => {
